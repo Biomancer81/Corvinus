6049985e85bb4e53bd3f45e170f9a6ebbdca6d59
Jacob Odea <v-jaodea@microsoft.com>
2019-05-02 12:07:31 -0700

    Added ExtendedQueue Class.

---
+++ src/Corvinus.Collections.Generic/QueueEventArgs.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Corvinus.Collections.Generic
+{
+    using System;
+
+    public class QueueEventArgs : EventArgs
+    {
+        public QueueEventArgs()
+        {
+            IsValid = false;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueueEventArgs"/> class.
+        /// </summary>
+        /// <param name="transactionValid">A valid Enqueue or Dequeue transaction has occurred.</param>
+        public QueueEventArgs(bool isValid)
+        {
+            IsValid = isValid;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a valid item has been enqueued or dequeued.
+        /// </summary>
+        public bool IsValid
+        {
+            get;
+            set;
+        }
+    }
+}
